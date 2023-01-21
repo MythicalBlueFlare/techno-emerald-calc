368a05c31ca7a02aa5507d71a622b411d0cabdf7
MythicalBlueFlare <112895815+MythicalBlueFlare@users.noreply.github.com>
2023-01-21 18:16:33 -0500

    Initial dist subtree commit

---
+++ dist/js/data/sets/gen9.js
@@ -0,0 +1,2 @@
+/* AUTOMATICALLY GENERATED FROM @smogon/sets, DO NOT EDIT! */
+var SETDEX_SV = {"Abomasnow":{"UU Showdown Usage":{"level":100,"ability":"Snow Warning","item":"Icy Rock","nature":"Modest","evs":{"hp":248,"sa":252,"sd":8},"moves":["Aurora Veil","Blizzard","Giga Drain","Leech Seed"]}}};
